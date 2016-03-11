774d926addf11e906495f420004dcfcebc968926
Karissa <whiting@ecohealthalliance.org>
2016-03-11 16:29:03 -0500

    adding files

---
+++ SIM_glm_curr.R
@@ -0,0 +1,35 @@
+source("SIM_join_case_curr.R")
+
+#fit function to sum_occur and case_count relationship- code
+fit<- glm(case_count ~ sum_occur, data = merge2, family=poisson())
+summary(fit)
+
+fitstate<- glm(case_count ~ sum_occur, data=mergestate, family = poisson())
+summary(fitstate)
+
+#turn fit function into linear model
+fit<- lm(fit, data=merge2)
+#fitstate< lm(fitstate, data=mergestate)-- NO PLOT YET FOR STATE DATA
+
+#create values of sum_occur for plot
+range(merge2$sum_occur)
+xw<- seq(0,3716, .01)
+yw<- predict(fit, list(sum_occur= xw, type="response")
+             
+             
+             
+             #omit zeros and NAs for plot
+             is.na(merge2[merge2==0] ) <- TRUE
+             merge3<-na.omit(merge2)
+             
+             #plot fit fucnction
+             plot(merge3$sum_occur, merge2$case_counts, pch = 16)
+             
+             #plot residuals
+             plot(fit)
+             
+             
+             
+             
+             
+             
---
+++ SIM_join_case_curr.R
@@ -0,0 +1,58 @@
+source("Sim_extract_us_curr.R")
+
+#read in latest zika case info and subset. newest case data should be renamed current_zika_cases and injested 
+zikacasesraw <- read.csv("data/zika_cases_curr.csv", header = TRUE, sep = ",")
+zikacases<-zikacasesraw[,c(9,10)]
+zikacases<- data.frame(lapply(zikacases, as.character), stringsAsFactors=FALSE)
+
+#rename column
+colnames(zikacases)[1]<- "Code"
+
+#create row of 1's and make numeric class
+zikacases$Count<- 1
+zikacases$Count<-as.numeric(zikacases$Count)
+
+#aggregate 1's ... should be count of cases by airport 
+z_case_code<-zikacases[,c(1,3)]
+z_case_code<-aggregate(Count ~ Code, data=z_case_code, sum)
+
+#aggregate 1's by state
+z_case_state<- aggregate(Count ~ State, data=zikacases, sum)
+
+#omit blanks -state and code
+z_case_code[z_case_code==""]<-NA
+z_case_code<-na.omit(z_case_code)
+
+z_case_state[z_case_state==""]<-NA
+z_case_state<-na.omit(z_case_state)
+
+
+#full join (i think) by code and state to compare US sim results and case spreadsheet
+require(dplyr)
+merge2<- full_join(usonlysimcurr_met_agg, z_case_code)
+mergestate<-full_join(usonlysimcurr_agg_state, z_case_state)
+
+
+#rename columns -state and code
+names(merge2)[3]<-"case_count"
+names(merge2)[2]<-"sum_occur"
+names(merge2)[1]<-"code"
+
+
+names(mergestate)[3]<-"case_count"
+names(mergestate)[2]<-"sum_occur"
+names(mergestate)[1]<-"state"
+
+#turn all na's to 0 - state and code
+merge2[ is.na(merge2) ] <- 0
+mergestate[ is.na(mergestate) ]<- 0
+
+#change class to numeric -state and code
+merge2$sum_occur<-as.numeric(merge2$sum_occur)
+class(merge2$sum_occur)
+write.csv(merge2, file="data/regions_curr.csv")
+
+mergestate$sum_occur<-as.numeric(mergestate$sum_occur)
+class(mergestate$sum_occur)
+write.csv(mergestate, file="data/state_curr.csv")
+
