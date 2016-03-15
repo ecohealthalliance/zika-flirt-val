3a027c4cef7d2b64b29e7c3d928c513a38f4bb45
Toph Allen <toph.allen@gmail.com>
2016-03-15 15:23:58 -0400

    Wrote script to run glms on all tables

---
+++ glms.R
@@ -0,0 +1,16 @@
+library(purrr)
+library(dplyr)
+
+options(stringsAsFactors = FALSE)
+
+sink("output/glms.txt")
+cat("Tests run in the following order:\n\n")
+list.files("data", pattern = "state|region", full.names = TRUE)
+cat("\n")
+cat("\n")
+list.files("data", pattern = "state|region", full.names = TRUE) %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / (2 * sd(seats)))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = poisson())) %>%
+  map(summary)
+sink()
