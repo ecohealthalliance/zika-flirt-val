90b479a07d7ca9b6da5fb14fd8c55343294dfa03
Toph Allen <toph.allen@gmail.com>
2016-03-17 00:49:21 -0400

    Played around with different model parameterizations. We're just gonna use Gaussian, screw the residuals.

--- glm playground.R
+++ glm playground.R
@@ -13,3 +13,106 @@ qplot(log(x), log(y), geom = "jitter")
 glm(y ~ x, family = poisson()) %>% summary()
 glm(y ~ x, family = gaussian()) %>% summary()
 
+
+
+
+
+
+
+# sink("output/glms-poisson.txt")
+# cat("Tests run in the following order:\n\n")
+# list.files("data", pattern = "state|region", full.names = TRUE)
+# cat("\n")
+# cat("\n")
+# list.files("data", pattern = "state|region", full.names = TRUE) %>%
+#   map(read.csv) %>%
+#   map(~ mutate(.x, seats = seats / (2 * sd(seats)))) %>%
+#   map(~ glm(case_count ~ seats, data = .x, family = poisson())) %>%
+#   map(summary)
+# sink()
+
+
+# sink("output/glms-gaussian.txt")
+# cat("Tests run in the following order:\n\n")
+# list.files("data", pattern = "state|region", full.names = TRUE)
+# cat("\n")
+# cat("\n")
+# list.files("data", pattern = "state|region", full.names = TRUE) %>%
+#   map(read.csv) %>%
+#   map(~ mutate(.x, seats = seats / (2 * sd(seats)))) %>%
+#   map(~ glm(case_count ~ seats, data = .x, family = gaussian())) %>%
+#   map(summary)
+# sink()
+
+
+
+m1 <- list.files("data", pattern = "state|region", full.names = TRUE)[c(1, 3)] %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = gaussian()))
+
+m2 <- list.files("data", pattern = "state|region", full.names = TRUE)[c(1, 3)] %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = poisson()))
+
+m3 <- list.files("data", pattern = "state|region", full.names = TRUE)[c(1, 3)] %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = poisson(link = "identity")))
+
+m4 <- list.files("data", pattern = "state|region", full.names = TRUE)[c(1, 3)] %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ log(seats), data = .x, family = poisson()))
+
+
+csvs <- m1 <- list.files("data", pattern = "state|region", full.names = TRUE)[c(1, 3)] %>%
+  map(read.csv)
+
+region <- csvs[[1]]
+
+reg.gaus <- glm(case_count ~ seats, data = region, family = gaussian())
+reg.pois <- glm(case_count ~ seats, data = region, family = poisson())
+reg.pois.id <- glm(case_count ~ seats, data = region, family = poisson(link = "identity"))
+
+fortify(reg.gaus) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
+
+fortify(reg.pois) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
+# Uh, yeah, not Poisson.
+
+fortify(reg.pois.id) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
+
+
+
+
+state <- csvs[[2]]
+
+sta.gaus <- glm(case_count ~ seats, data = state, family = gaussian())
+sta.pois <- glm(case_count ~ seats, data = state, family = poisson())
+sta.pois.id <- glm(case_count ~ seats, data = state, family = poisson(link = "identity"))
+
+fortify(sta.gaus) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
+
+fortify(sta.pois) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
+# Uh, yeah, not Poisson.
+
+fortify(sta.pois.id) %>%
+  ggplot() +
+  geom_point(aes(x = seats, y = case_count)) +
+  geom_line(aes(x = seats, y = .fitted))
--- glms.R
+++ glms.R
@@ -3,6 +3,19 @@ library(dplyr)
 
 options(stringsAsFactors = FALSE)
 
+sink("output/glms-gaussian.txt")
+cat("Tests run in the following order:\n\n")
+list.files("data", pattern = "state|region", full.names = TRUE)
+cat("\n")
+cat("\n")
+list.files("data", pattern = "state|region", full.names = TRUE) %>%
+  map(read.csv) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = gaussian())) %>%
+  map(summary)
+sink()
+
+
 sink("output/glms-poisson.txt")
 cat("Tests run in the following order:\n\n")
 list.files("data", pattern = "state|region", full.names = TRUE)
@@ -10,20 +23,7 @@ cat("\n")
 cat("\n")
 list.files("data", pattern = "state|region", full.names = TRUE) %>%
   map(read.csv) %>%
-  map(~ mutate(.x, seats = seats / (2 * sd(seats)))) %>%
-  map(~ glm(case_count ~ seats, data = .x, family = poisson())) %>%
-  map(summary)
-sink()
-
-
-sink("output/glms-gaussians.txt")
-cat("Tests run in the following order:\n\n")
-list.files("data", pattern = "state|region", full.names = TRUE)
-cat("\n")
-cat("\n")
-list.files("data", pattern = "state|region", full.names = TRUE) %>%
-  map(read.csv) %>%
-  map(~ mutate(.x, seats = seats / (2 * sd(seats)))) %>%
-  map(~ glm(case_count ~ seats, data = .x, family = gaussian())) %>%
+  map(~ mutate(.x, seats = seats / sd(seats))) %>%
+  map(~ glm(case_count ~ seats, data = .x, family = poisson(link = "identity"))) %>%
   map(summary)
 sink()
