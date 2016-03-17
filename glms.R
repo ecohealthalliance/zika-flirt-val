f09d28b30c5e844a2a6f4136c213ccf88f6129b0
Toph Allen <toph.allen@gmail.com>
2016-03-17 01:02:04 -0400

    So I removed the 'unknown' lines and now one of the poisson(link = 'identity' models isn't fitting. Gaussian it is.

--- glms.R
+++ glms.R
@@ -5,25 +5,26 @@ options(stringsAsFactors = FALSE)
 
 sink("output/glms-gaussian.txt")
 cat("Tests run in the following order:\n\n")
-list.files("data", pattern = "state|region", full.names = TRUE)
+list.files("data", pattern = "past|curr", full.names = TRUE)
 cat("\n")
 cat("\n")
-list.files("data", pattern = "state|region", full.names = TRUE) %>%
+list.files("data", pattern = "past|curr", full.names = TRUE) %>%
   map(read.csv) %>%
   map(~ mutate(.x, seats = seats / sd(seats))) %>%
   map(~ glm(case_count ~ seats, data = .x, family = gaussian())) %>%
   map(summary)
 sink()
 
+# This one isn't running right now
 
-sink("output/glms-poisson.txt")
-cat("Tests run in the following order:\n\n")
-list.files("data", pattern = "state|region", full.names = TRUE)
-cat("\n")
-cat("\n")
-list.files("data", pattern = "state|region", full.names = TRUE) %>%
-  map(read.csv) %>%
-  map(~ mutate(.x, seats = seats / sd(seats))) %>%
-  map(~ glm(case_count ~ seats, data = .x, family = poisson(link = "identity"))) %>%
-  map(summary)
-sink()
+# sink("output/glms-poisson.txt")
+# cat("Tests run in the following order:\n\n")
+# list.files("data", pattern = "past|curr", full.names = TRUE)
+# cat("\n")
+# cat("\n")
+# list.files("data", pattern = "past|curr", full.names = TRUE) %>%
+#   map(read.csv) %>%
+#   map(~ mutate(.x, seats = seats / sd(seats))) %>%
+#   map(~ glm(case_count ~ seats, data = .x, family = poisson(link = "identity"))) %>%
+#   map(summary)
+# sink()
